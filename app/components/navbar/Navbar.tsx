00369fa1aa9f3a681c441524add16a24764078fe
yeferson321 <131elyefer@gmail.com>
2023-12-03 11:46:28 -0500

    version 1.1

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -8,22 +8,7 @@ interface ProspData {
 
 export default function Example({ fullName }: ProspData) {
 
-  const copyUrlShare = () => {
-    if (window.isSecureContext && navigator.clipboard) {
-      navigator.clipboard.writeText(window.location.href).then(
-        () => {
-          console.log("Data was shared successfully");
-          alert("se corecto")
-        },
-        () => {
-          console.error("error:");
-          alert("hubo un errir")
-        },
-      )
-    } else {
-      alert("no es seguro")
-    }
-  };
+
 
   return (
     <nav className="bg-black">
@@ -43,7 +28,7 @@ export default function Example({ fullName }: ProspData) {
         </div>
 
         <div className="flex">
-          <button type="button" onClick={copyUrlShare}>
+          <button type="button" >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6 stroke-white hover:stroke-slate-300">
               <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
             </svg>
@@ -51,6 +36,7 @@ export default function Example({ fullName }: ProspData) {
         </div>
 
       </div>
+      <Modal/>
     </nav >
   )
 }
